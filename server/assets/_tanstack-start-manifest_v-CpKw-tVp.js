58e6b88127c79385c09dcace9e9d786fbbd0641b
yectra <srikumaran@yectra.com>
2026-05-14 15:29:14 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-CpKw-tVp.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Portfolio-SK/portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DiQ75toV.js"] }, "/": { filePath: "C:/Portfolio-SK/portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DOpzZsa4.js"] } }, clientEntry: "/assets/index-DiQ75toV.js" });
-export {
-  tsrStartManifest
-};
